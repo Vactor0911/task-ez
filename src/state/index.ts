a4f9739510683e20443d8887094c59d9cb592ef0
Vactor0911 <vactor0911@gmail.com>
2024-11-30 22:23:20 +0900

    커스텀 더보기 버튼 구현

--- src/state/index.ts
+++ src/state/index.ts
@@ -2,3 +2,7 @@ import dayjs from "dayjs";
 import { atom } from "jotai";
 
 export const selectedDateAtom = atom(dayjs());
+
+export const hiddenEventsAtom = atom<any[]>([]);
+export const isShowMoreOpenedAtom = atom(false);
+export const showMoreBtnAnchorAtom = atom<HTMLButtonElement | null>(null);
