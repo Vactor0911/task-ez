d05fd9de518a92275565b817272dcd1adffd6da1
Vactor0911 <vactor0911@gmail.com>
2024-12-03 16:00:54 +0900

    날짜 선택 최소, 최대 범위 재조정

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,5 +7,5 @@ moment.locale("ko-KR");
 export const localizer = momentLocalizer(moment);
 
 // 선택 가능한 날짜 범위
-export const MIN_DATE = dayjs().add(-1, "year").month(0); // 1년 전 1월
-export const MAX_DATE = dayjs().add(1, "year").month(11); // 1년 후 12월
+export const MIN_DATE = dayjs().add(-1, "year").month(0).startOf("month"); // 1년 전 1월
+export const MAX_DATE = dayjs().add(1, "year").month(11).endOf("month"); // 1년 후 12월
