23dbd3e8b9736a2d82fb76cd338f3dedd11ecf25
Vactor0911 <vactor0911@gmail.com>
2024-12-05 10:58:18 +0900

    dev 코드 리뷰

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -13,7 +13,12 @@ import { Close as CloseIcon } from "@mui/icons-material";
 import { color } from "../utils/theme";
 import dayjs from "dayjs";
 import { useAtom, useAtomValue } from "jotai";
-import { eventsAtom, isModalOpenedAtom, serverInfoAtom, taskModalDataAtom } from "../state"; // serverInfoAtom - API 통신을 위한 서버 정보
+import {
+  eventsAtom,
+  isModalOpenedAtom,
+  serverInfoAtom,
+  taskModalDataAtom,
+} from "../state"; // serverInfoAtom - API 통신을 위한 서버 정보
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { MAX_DATE, MIN_DATE } from "../utils";
@@ -84,51 +89,51 @@ const TaskModal = () => {
         )
       );
     }
-        
-//    axios
-//      .post(`${HOST}:${PORT}/api/saveTask`, taskData)
-//      .then((response) => {
-//        if (response.data.success) {
-//          console.log("작업 저장 성공:", response.data);
-//          alert("작업이 성공적으로 저장되었습니다.");
-//
-//          // 새로 저장된 작업의 ID를 프론트엔드 상태에 반영
-//          if (taskModalData.id === -1) {
-//            setEvents((prevEvents) => [
-//              ...prevEvents,
-//              {
-//                ...taskData,
-//                id: response.data.task_id, // 백엔드에서 반환된 고유 ID
-//                start: new Date(taskData.start), // 문자열을 Date 객체로 변환
-//                end: new Date(taskData.end), // 문자열을 Date 객체로 변환
-//              },
-//            ]);
-//          }
-//        } else {
-//          console.error("작업 저장 실패:", response.data.message);
-//          alert("작업 저장에 실패했습니다: " + response.data.message);
-//        }
-//      })
-//      .catch((error) => {
-//        console.error("작업 저장 중 오류 발생:", error);
-//        alert("작업 저장 중 오류가 발생했습니다. 다시 시도해주세요.");
-//      });
-        
-    setIsModalOpened(false);
-  }, [events, title, currentColor, description, startDate, endDate]);
 
-//    // 백엔드 API 호출
-//    const taskData = {
-//      id: taskModalData.id || null, // 고유 ID (새 작업인 경우 null로 전송)
-//      user_id: 2, // 임시 사용자 ID
-//      title,
-//      description,
-//      start: startDate.toISOString(),
-//      end: endDate.add(1, "day").toISOString(),
-//      color: currentColor,
-//    };
-
+    // TODO: 백엔드 통신 부분 작업 오브젝트 형식 일치화 필요
+      //  axios
+      //    .post(`${HOST}:${PORT}/api/saveTask`, taskData)
+      //    .then((response) => {
+      //      if (response.data.success) {
+      //        console.log("작업 저장 성공:", response.data);
+      //        alert("작업이 성공적으로 저장되었습니다.");
     
+      //        // 새로 저장된 작업의 ID를 프론트엔드 상태에 반영
+      //        if (taskModalData.id === -1) {
+      //          setEvents((prevEvents) => [
+      //            ...prevEvents,
+      //            {
+      //              ...taskData,
+      //              id: response.data.task_id, // 백엔드에서 반환된 고유 ID
+      //              start: new Date(taskData.start), // 문자열을 Date 객체로 변환
+      //              end: new Date(taskData.end), // 문자열을 Date 객체로 변환
+      //            },
+      //          ]);
+      //        }
+      //      } else {
+      //        console.error("작업 저장 실패:", response.data.message);
+      //        alert("작업 저장에 실패했습니다: " + response.data.message);
+      //      }
+      //    })
+      //    .catch((error) => {
+      //      console.error("작업 저장 중 오류 발생:", error);
+      //      alert("작업 저장 중 오류가 발생했습니다. 다시 시도해주세요.");
+      //    });
+
+    setIsModalOpened(false);
+  }, [events, title, currentColor, description, startDate, endDate]); // TODO: taskData 값 형식 변경 후 디펜던시 추가 필요
+
+  // TODO: taskData 값 형식을 TaskProps 인터페이스와 일치화 필요
+  //    // 백엔드 API 호출
+  //    const taskData = {
+  //      id: taskModalData.id || null, // 고유 ID (새 작업인 경우 null로 전송)
+  //      user_id: 2, // 임시 사용자 ID
+  //      title,
+  //      description,
+  //      start: startDate.toISOString(),
+  //      end: endDate.add(1, "day").toISOString(),
+  //      color: currentColor,
+  //    };
 
   // 삭제 버튼 클릭
   const handleDeleteButtonClicked = useCallback(() => {
@@ -159,9 +164,11 @@ const TaskModal = () => {
         </Box>
 
         {/* 구분선 */}
-        <Divider sx={{
-          mb: 1,
-        }} />
+        <Divider
+          sx={{
+            mb: 1,
+          }}
+        />
 
         {/* 날짜 필드 */}
         <Box sx={{ display: "flex", justifyContent: "space-between" }}>
