31dbe42b8416c288aac72a3fc74c1a15a21ec665
Vactor0911 <vactor0911@gmail.com>
2024-12-07 17:17:29 +0900

    날짜를 한국 표준시로 변경하는 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import moment from "moment";
 import { momentLocalizer } from "react-big-calendar";
 
 //서버 정보
-export const SERVER_HOST = "http://task-ez.vactor0911.dev";
+export const SERVER_HOST = "https://task-ez.vactor0911.dev";
 
 // 한국어 설정
 moment.locale("ko-KR");
@@ -12,3 +12,8 @@ export const localizer = momentLocalizer(moment);
 // 선택 가능한 날짜 범위
 export const MIN_DATE = dayjs().add(-1, "year").month(0).startOf("month"); // 1년 전 1월
 export const MAX_DATE = dayjs().add(1, "year").month(11).endOf("month"); // 1년 후 12월
+
+// toISOString() 반환값을 한국 표준시로 변환
+export const toKstISOString = (date: dayjs.Dayjs) => {
+    return new Date(date.toDate().getTime() - date.toDate().getTimezoneOffset() * 60000).toISOString();
+};
