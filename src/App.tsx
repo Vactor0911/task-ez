debbffc39e50165837ddfff71910c5216f52704b
Vactor0911 <vactor0911@gmail.com>
2024-12-09 18:43:54 +0900

    모바일 접속 화면 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/Main";
+import Mobile from "./pages/Mobile";
 
 function App() {
   return (
     <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={screen.width < 768 ? <Mobile /> : <Main />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
