f01f542f0d396b1045b4aa993155a3d911401b48
Vactor0911 <vactor0911@gmail.com>
2024-12-12 00:02:14 +0900

    작업 알림 범위 오류 수정 및 종료일 임박순 정렬

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,5 +20,5 @@ export const toKstISOString = (date: dayjs.Dayjs) => {
 
 // 한국 표준시가 적용된 dayjs 객체 반환
 export const koDayjs = (date?: dayjs.Dayjs) => {
-    return dayjs(dayjs(date).toDate().getTime() - dayjs(date).toDate().getTimezoneOffset() * 60000);
+    return dayjs(dayjs(date).toDate());
 };
